6b010ec392711784efb3f5b3a65ff379a5731a08
biwin <biwin007@gmail.com>
2014-09-29 23:52:41 +0530

    create tehe robo

---
+++ src/com/twenty/robo.java
@@ -0,0 +1,28 @@
+package com.twenty;
+
+public class robo {
+	String status;
+	int speed;
+	float temperature;
+
+
+	void checkTemperature(){
+		if (temperature > 660){
+			status = "returning home!";
+			speed = 5;
+		}
+		else if (temperature < 400){
+			status = "I am freezing here!! :(";
+			speed = 1;
+		}
+		else {
+			status = "Happy here, dont disturb me";
+			speed = 3;
+		}
+	}
+	void showAttributes(){
+		System.out.println("Status: " + status);
+		System.out.println("Speed: " + speed);
+		System.out.println("Temperature: " + temperature);
+	}
+}
